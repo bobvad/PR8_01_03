b48e46b4c992af9542a1f72dd8d68650e74b375a
Djfjfj2006@outlook.com <Djfjfj2006@outlook.com>
2025-10-08 00:13:39 +0500

    Программирование MainActivity.Дегтянников.

--- app/src/main/java/com/example/notes_degtiannikov/MainActivity.java
+++ app/src/main/java/com/example/notes_degtiannikov/MainActivity.java
@@ -1,24 +1,94 @@
 package com.example.notes_degtiannikov;
 
+import android.graphics.Color;
 import android.os.Bundle;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.MultiAutoCompleteTextView;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
+    public List<Notes> AllNotes = new ArrayList<>();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+
+    }
+    public void OnAddNote(View view)
+    {
+        setContentView(R.layout.activity_note);
+    }
+    public void AddNote(View view)
+    {
+        EditText etName = findViewById(R.id.etName);
+        MultiAutoCompleteTextView etText = findViewById(R.id.etText);
+        Notes newNote = new Notes();
+        newNote.Name = etName.getText().toString();
+        newNote.Text = etText.getText().toString();
+        newNote.Date = new SimpleDateFormat("dd.MM.yyyy").format(new Date());
+        AllNotes.add(newNote);
+        setContentView(R.layout.activity_main);
+         OnLoad();
+    }
+    public void OnLoad()
+    {
+        LinearLayout llParent = findViewById(R.id.llParent);
+        llParent.removeAllViews();
+
+
+
+        for (int iNote = 0; iNote < AllNotes.size(); iNote ++)
+        {
+            LinearLayout LL = new LinearLayout(this);
+            LL.setOrientation(LinearLayout.HORIZONTAL);
+            RelativeLayout.LayoutParams Params = new RelativeLayout.LayoutParams(
+                    ViewGroup.LayoutParams.MATCH_PARENT,
+                    ViewGroup.LayoutParams.MATCH_PARENT
+            );
+            LL.setLayoutParams(Params);
+            ImageView Logo = new ImageView(this);
+            RelativeLayout.LayoutParams LogoPramas = new RelativeLayout.LayoutParams(150,150);
+            Logo.setLayoutParams(LogoPramas);
+            Logo.setImageResource(R.drawable.icon);
+            Logo.setPadding(20,20,20,20);
+            LinearLayout LL_Vertical = new LinearLayout(this);
+            LL_Vertical.setLayoutParams(Params);
+            LL_Vertical.setOrientation(LinearLayout.VERTICAL);
+
+            TextView tvName = new TextView(this);
+            tvName.setText(AllNotes.get(iNote).Name);
+
+            RelativeLayout.LayoutParams tvParams = new RelativeLayout.LayoutParams(
+                    ViewGroup.LayoutParams.MATCH_PARENT,
+                    ViewGroup.LayoutParams.WRAP_CONTENT
+            );
+            tvName.setLayoutParams(tvParams);
+            tvName.setTextColor(Color.BLACK);
+            tvName.setTextSize(18);
+
+            TextView tvDate = new TextView(this);
+            tvDate.setText(AllNotes.get(iNote).Name);
+            tvDate.setLayoutParams(tvParams);
+            tvDate.setTextColor(Color.GRAY);
+
+            llParent.addView(LL);
+            LL.addView(Logo);
+            LL.addView(LL_Vertical);
+            LL_Vertical.addView(tvName);
+            LL_Vertical.addView(tvDate);
+        }
     }
 }
---
+++ app/src/main/java/com/example/notes_degtiannikov/Notes.java
@@ -0,0 +1,7 @@
+package com.example.notes_degtiannikov;
+
+public class Notes {
+    public String Name;
+    public String Text;
+    public String Date;
+}
